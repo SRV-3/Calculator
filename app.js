935b1e0e9197b22116bf22e3427d29f7d39bfe1f
Sourav Singh <thakursourav778@gmail.com>
2024-10-03 19:47:36 +0530

    problem fixed

--- app.js
+++ app.js
@@ -2,32 +2,32 @@ let displayValue = '';
 
 function clearDisplay() {
     displayValue = '';
-    document.querySelector('box').value = displayValue;
+    document.querySelector('.box').value = displayValue;
 }
 
 function deleteLast() {
     displayValue = displayValue.slice(0, -1);
-    document.querySelector('box').value = displayValue;
+    document.querySelector('.box').value = displayValue;
 }
 
 function appendNumber(number) {
     displayValue += number;
-    document.querySelector('box').value = displayValue;
+    document.querySelector('.box').value = displayValue;
     
 }
 
 function appendOperator(operator) {
     if (displayValue === '' && operator !== '-') return;  // Prevent adding operator at the beginning
     displayValue += operator;
-    document.querySelector('box').value = displayValue;
+    document.querySelector('.box').value = displayValue;
 }
 
 function calculate() {
     try {
         displayValue = eval(displayValue);  // Using eval for simplicity
-        document.querySelector('box').value = displayValue;
+        document.querySelector('.box').value = displayValue;
     } catch (error) {
-        document.querySelector('box').value = 'Error';
+        document.querySelector('.box').value = 'Error';
         displayValue = '';
     }
 }
